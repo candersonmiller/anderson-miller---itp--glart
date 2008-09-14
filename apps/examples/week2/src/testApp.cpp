f1e7898de38aad391a9d7d26f7800c3bbd7a0c6b
C. Anderson Miller <candersonmiller@gmail.com>
2008-09-14 15:52:33 -0400

    working projection, mars orbitting around sun

--- apps/examples/week2/src/testApp.cpp
+++ apps/examples/week2/src/testApp.cpp
@@ -16,9 +16,34 @@ void testApp::update(){
 
 //--------------------------------------------------------------
 void testApp::draw(){	
-	//glMatrixMode(GL_PROJECTION);
+	glMatrixMode(GL_PROJECTION);
+	//glLoadIdentity();
+	glEnable(GL_DEPTH_TEST);
 	//Mars
-	glLoadIdentity();
+	//glLoadIdentity();
+	
+	glTranslated(700,-300,-100);
+	
+	glPushMatrix();
+	glTranslated(-225,0,0);
+	glColor3f(0.9,0.9,0.00);
+	glutSolidSphere(150,50,50);
+	
+	glPopMatrix();
+	glColor3f(0.7, 0.2 ,0);
+
+	glRotatef(counter, -0.3f, 1.0f,0);
+	glTranslated(100,100,0); //away from "origin"
+	glutSolidSphere(50,100,100);
+	
+	//glPopMatrix();
+	//glColor3f(1,1,1);
+	//glTranslated(-50,0,0);
+	//glRotatef(counter-20, 0, 1.0f, 0);
+	//glutSolidSphere(30,40,40);
+	
+	
+	/*glLoadIdentity();
 	glTranslated(400,400,-100);
 	glColor3f(0.7, 0.2 ,0);
 	glRotatef(counter, -0.3f, 1.0f, 0);
@@ -30,7 +55,7 @@ void testApp::draw(){
 	glColor3f(0.9,0.9,0.00);
 	glTranslated(400,400,-300);
 	glRotatef(counter, 0.0f,1.0f,0.0f);
-	glutSolidSphere(200,50,50);
+	glutSolidSphere(200,50,50);*/
 	
 
 	
