c87aff4c3e31fadc76f8a34ef0046739d737f806
C. Anderson Miller <candersonmiller@gmail.com>
2008-10-28 15:01:41 -0400

    added image write function

--- midterm/opengl.py
+++ midterm/opengl.py
@@ -4,126 +4,35 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import random
+import os
 import sys
 import math
+import Image
 from numpy import *
 
 
-## To Do:
-#  		Commit to Git Repository
+####### To Do:
+#  		(done) Commit to Git Repository
 #  		Tweak Lighting
 #		Tweak Colors
-#		Sun
-#       Image file for frame
-#		Basic Rotation
-
+#		Sun (done, tweaking)
+#       Image file for frame (xcode working)
+#		Basic Rotation (done)
+#       Green Grass (as per Tom's suggestion)
+#       (done) sinewave()
+imagecounter = 1000
 counter = 0
 cloudsize = 15
 xrand = [cloudsize]
 yrand = [cloudsize]
 zrand = [cloudsize]
 
-period = 10
+period = 40
 
 for i in range(0, cloudsize):
 	xrand.append(0)
 	yrand.append(0)
 	zrand.append(0)
-
-#from http://www.fundza.com/c4serious/noise/perlin/perlin.html
-p = (
-151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,140,36,103,
-30,69,142,8,99,37,240,21,10,23,190,6,148,247,120,234,75,0,26,197,
-62,94,252,219,203,117,35,11,32,57,177,33,88,237,149,56,87,174,20,
-125,136,171,168,68,175,74,165,71,134,139,48,27,166,77,146,158,231,
-83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,102,
-143,54,65,25,63,161,1,216,80,73,209,76,132,187,208,89,18,169,200,
-196,135,130,116,188,159,86,164,100,109,198,173,186,3,64,52,217,226,
-250,124,123,5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,
-58,17,182,189,28,42,223,183,170,213,119,248,152,2,44,154,163,70,
-221,153,101,155,167,43,172,9,129,22,39,253,19,98,108,110,79,113,
-224,232,178,185,112,104,218,246,97,228,251,34,242,193,238,210,144,
-12,191,179,162,241,81,51,145,235,249,14,239,107,49,192,214,31,181,
-199,106,157,184,84,204,176,115,121,50,45,127,4,150,254,138,236,
-205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180,
-151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,140,36,103,
-30,69,142,8,99,37,240,21,10,23,190,6,148,247,120,234,75,0,26,197,
-62,94,252,219,203,117,35,11,32,57,177,33,88,237,149,56,87,174,20,
-125,136,171,168,68,175,74,165,71,134,139,48,27,166,77,146,158,231,
-83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,102,
-143,54,65,25,63,161,1,216,80,73,209,76,132,187,208,89,18,169,200,
-196,135,130,116,188,159,86,164,100,109,198,173,186,3,64,52,217,226,
-250,124,123,5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,
-58,17,182,189,28,42,223,183,170,213,119,248,152,2,44,154,163,70,
-221,153,101,155,167,43,172,9,129,22,39,253,19,98,108,110,79,113,
-224,232,178,185,112,104,218,246,97,228,251,34,242,193,238,210,144,
-12,191,179,162,241,81,51,145,235,249,14,239,107,49,192,214,31,181,
-199,106,157,184,84,204,176,115,121,50,45,127,4,150,254,138,236,
-205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180)
-def lerp(t, a, b):
-    return a + t * (b - a)
-  
-def fade(t):
-    return t * t * t * (t * (t * 6 - 15) + 10)
-  
-def grad(hash, x, y, z):
-    h = hash & 15
-    if h < 8:
-        u = x
-    else:
-        u = y
-    if h < 4:
-        v = y
-    elif h == 12 or h == 14:
-        v = x
-    else:
-        v = z
-    if h & 1 != 0:
-        u = -u
-    if h & 2 != 0:
-        v = -v
-    return u + v
-  
-def pnoise(x, y, z):
-    global p
-    X = int(math.floor(x)) & 255
-    Y = int(math.floor(y)) & 255
-    Z = int(math.floor(z)) & 255
-    x -= math.floor(x)
-    y -= math.floor(y)
-    z -= math.floor(z)
-    
-    u = fade(x)
-    v = fade(y)
-    w = fade(z)
-    
-    A =  p[X] + Y
-    AA = p[A] + Z
-    AB = p[A + 1] + Z
-    B =  p[X + 1] + Y
-    BA = p[B] + Z
-    BB = p[B + 1] + Z
-    
-    pAA = p[AA]
-    pAB = p[AB]
-    pBA = p[BA]
-    pBB = p[BB]
-    pAA1 = p[AA + 1]
-    pBA1 = p[BA + 1]
-    pAB1 = p[AB + 1]
-    pBB1 = p[BB + 1]
-    
-    gradAA =  grad(pAA, x,   y,   z)
-    gradBA =  grad(pBA, x-1, y,   z)
-    gradAB =  grad(pAB, x,   y-1, z)
-    gradBB =  grad(pBB, x-1, y-1, z)
-    gradAA1 = grad(pAA1,x,   y,   z-1)
-    gradBA1 = grad(pBA1,x-1, y,   z-1)
-    gradAB1 = grad(pAB1,x,   y-1, z-1)
-    gradBB1 = grad(pBB1,x-1, y-1, z-1)
-    return lerp(w, 
-    lerp(v, lerp(u, gradAA, gradBA), lerp(u, gradAB, gradBB)),
-    lerp(v, lerp(u, gradAA1,gradBA1),lerp(u, gradAB1,gradBB1)))    
 
 
 # Some api in the chain is translating the keystrokes to this octal string
@@ -179,6 +88,7 @@ def ReSizeGLScene(Width, Height):
 
 # The main drawing function. 
 def DrawGLScene():
+	global imagecounter
 	global counter
 	global xrand 	#array containing positions of all spheres (x values)
 	global yrand	#array containing positions of all spheres (y values)
@@ -197,17 +107,29 @@ def DrawGLScene():
 	#ambient scene light
 	glPushMatrix() #Push
 	glTranslated(5, 4, -2)
-	ambient = (0.7, 0.7, 0.7, 1.0)
+	ambient = (0.55, 0.55, 0.55, 1.0)
 	glLightfv(GL_LIGHT1, GL_AMBIENT, ambient)
 	glPopMatrix() #Pop
 	
-
+	
 	
 	#sunlight
+	sun = 0  # 1 for on, 0 for off
+	if(sun):
+		glPushMatrix()
+		glTranslatef(-5, 3, -10)
+		glColor3f(255, 249, 78)
+		s1color = ( 1.0, 0.93, 0.27, 1.0 )
+		glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, s1color)
+		glMaterialf(GL_FRONT, GL_SHININESS, 1)
+		glutSolidSphere(1,100,100)
+		glPopMatrix()
+	
+	
 	position = (-5, 1,-10, 1)
 	specularLight = (0.7, 0.7, 0.7, 1)
-	glLightfv(GL_LIGHT0, GL_POSITION, position)
-	glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight)
+	glLightfv(GL_LIGHT2, GL_POSITION, position)
+	glLightfv(GL_LIGHT2, GL_SPECULAR, specularLight)
 
 
 	#fill light
@@ -221,7 +143,7 @@ def DrawGLScene():
 
 	glEnable(GL_LIGHT0)
 	glEnable(GL_LIGHT1)
-
+	glEnable(GL_LIGHT2)
 	
 	glLoadIdentity()			
 
@@ -229,9 +151,10 @@ def DrawGLScene():
 	
 	#print "%.2f" % floatingpt
 	glTranslate(-4.5, 0, 0) #jokey coming off screen effect
-	glTranslatef(floatingpt * 12 , 0, 0) #flyin' by
+	glTranslatef(floatingpt * 12 , math.sin((xrand[0] + floatingpt)*5) * 1, 0) #flyin' by
 	glTranslatef(-1.5,0.0,-6.0)				
-	if( counter > period or counter == 0):
+	if( counter > period or counter == 0):   #time to make a new cloud
+		#this is how you make a new cloud
 		for i in range(0, cloudsize):
 			xrand[i] = random.uniform(-0.5, 0.5)
 			yrand[i] = random.uniform(-0.5, 0.5)
@@ -241,14 +164,36 @@ def DrawGLScene():
 	else:
 		counter = counter + 0.03
 
+	##this is how we draw clouds
+	glPushMatrix() #push
+	glRotatef(20 * floatingpt, 1.0, 0.5, 0.0)
 	
 	for x in range(0,cloudsize):
 		glPushMatrix()  #Push
 		glTranslatef(xrand[x],yrand[x],zrand[x])
 		drawSphere()
 		glPopMatrix()   #Pop
+	glPopMatrix() #pop
 
+	size = 640, 480
+	towrite = Image.new('RGB', size)
+	pixels = glReadPixels(0, 0, 640, 480, GL_RGB, GL_FLOAT)
+	#print pixels[0][0][0]
+	#print pixels[0][0][1]
+	#print pixels[0][0][2]
+	imagecounter = imagecounter + 1
+	
+	for x in range(640):
+		for y in range(480):
+			red = int(pixels[x][y][0] * 255)
+			green = int(pixels[x][y][1] * 255)
+			blue = int(pixels[x][y][2] * 255)
 
+			towrite.putpixel((x,y),(red,green,blue))
+	outfile = 'frame00%d.jpg' % imagecounter
+	towrite.save(outfile, "JPEG")
+	
+	
 	glutSwapBuffers()
 
 # The function called whenever a key is pressed. Note the use of Python tuples to pass in: (key, x, y)  
@@ -289,7 +234,7 @@ def main():
 	#glutDisplayFunc()
 	
 	# Uncomment this line to get full screen.
-	glutFullScreen()
+	#glutFullScreen()
 
 	# When we are doing nothing, redraw the scene.
 	glutIdleFunc(DrawGLScene)
